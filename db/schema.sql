7e00ac3ebeebf90327f845cdb5fc47c909519554
DiogoS77 <diogo.messi.18@hotmail.com>
2023-06-30 00:11:31 +0100

    complete schema.sql by adding the rest create tabl

--- db/schema.sql
+++ db/schema.sql
@@ -7,3 +7,24 @@ CREATE TABLE IF NOT EXISTS departments (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30)
 );
+
+CREATE TABLE IF NOT EXISTS roles (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+        REFERENCES departments(id)
+        ON DELETE SET NULL
+);
+
+CREATE TABLE IF NOT EXISTS employees (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id)
+        REFERENCES roles(id)
+        ON DELETE SET NULL
+);
