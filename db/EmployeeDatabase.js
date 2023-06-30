5132974f9cf60d7f658ef0326beb5b52985f5efd
DiogoS77 <diogo.messi.18@hotmail.com>
2023-06-30 23:49:57 +0100

    fixed employeeDatabse, by adjusting the query

--- db/EmployeeDatabase.js
+++ db/EmployeeDatabase.js
@@ -5,9 +5,26 @@ class EmployeeDatabase extends Database {
         super(options);
     }
 
-    async getDepartments() {
+    async executeQuery(query, params) {
         try {
-            const results = await this.db.promise().query('SELECT * FROM department');
+            const results = await new Promise((resolve, reject) => {
+                this.db.query(query, params, (err, results) => {
+                    if (err) {
+                        reject(err);
+                    }
+                    resolve(results);
+                });
+            });
+            return results;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async getDepartments() {
+        const query = 'SELECT * FROM department';
+        try {
+            const results = await this.executeQuery(query);
             return results;
         } catch (error) {
             throw error;
@@ -15,12 +32,13 @@ class EmployeeDatabase extends Database {
     }
 
     async getRoles() {
+        const query = `
+            SELECT role.id, role.title, CONCAT('£', FORMAT(salary, 0), 'p/a') as salary, department.name as department_name
+            FROM role
+            INNER JOIN department ON role.department_id = department.id
+        `;
         try {
-            const results = await this.db.promise().query(`
-                SELECT role.id, role.title, CONCAT('£', FORMAT(salary, 0), 'p/a') as salary, department.name as department_name
-                FROM role
-                INNER JOIN department ON role.department_id = department.id
-            `);
+            const results = await this.executeQuery(query);
             return results;
         } catch (error) {
             throw error;
@@ -28,20 +46,21 @@ class EmployeeDatabase extends Database {
     }
 
     async getEmployees() {
+        const query = `
+            SELECT
+            employee.id,
+            CONCAT(employee.first_name, ' ', employee.last_name) as name,
+            role.title as role_title,
+            role.salary as role_salary,
+            department.name as department_name,
+            IF(CONCAT(manager.first_name, ' ', manager.last_name) IS NULL, '', CONCAT(manager.first_name, ' ', manager.last_name)) as manager_name
+            FROM employee
+            INNER JOIN role ON employee.role_id = role.id
+            INNER JOIN department ON role.department_id = department.id
+            LEFT JOIN employee as manager ON employee.manager_id = manager.id
+        `;
         try {
-            const results = await this.db.promise().query()(`
-                SELECT
-                employee.id,
-                CONCAT(employee.first_name, ' ', employee.last_name) as name,
-                role.title as role_title,
-                role.salary as role_salary,
-                department.name as department_name,
-                IF(CONCAT(manager.first_name, ' ', manager.last_name) IS NULL, '', CONCAT(manager.first_name, ' ', manager.last_name)) as manager_name
-                FROM employee
-                INNER JOIN role ON employee.role_id = role.id
-                INNER JOIN department ON role.department_id = department.id
-                LEFT JOIN employee as manager ON employee.manager_id = manager.id
-            `);
+            const results = await this.executeQuery(query);
             return results;
         } catch (error) {
             throw error;
@@ -49,8 +68,10 @@ class EmployeeDatabase extends Database {
     }
 
     async addDepartment(department) {
+        const query = 'INSERT INTO department SET ?';
+        const params = { name: department.department_name };
         try {
-            const results = await this.db.promise().query('INSERT INTO department SET ?', { name: department.department_name });
+            await this.executeQuery(query, params);
             return `Department ${department.department_name} added successfully`;
         } catch (error) {
             throw error;
@@ -58,14 +79,14 @@ class EmployeeDatabase extends Database {
     }
 
     async addRole(role) {
-        const roleData = {
+        const query = 'INSERT INTO role SET ?';
+        const params = {
             title: role.title,
             salary: role.salary,
             department_id: role.department_id
         };
-
         try {
-            const results = await this.db.promise().query('INSERT INTO role SET ?', roleData);
+            await this.executeQuery(query, params);
             return `Role ${role.title} added successfully`;
         } catch (error) {
             throw error;
@@ -73,15 +94,15 @@ class EmployeeDatabase extends Database {
     }
 
     async addEmployee(employee) {
-        const employeeData = {
+        const query = 'INSERT INTO employee SET ?';
+        const params = {
             first_name: employee.first_name,
             last_name: employee.last_name,
             role_id: employee.role_id,
             manager_id: employee.manager_id
         };
-
         try {
-            const results = await this.db.promise().query('INSERT INTO employee SET ?', employeeData);
+            await this.executeQuery(query, params);
             return `${employee.first_name} ${employee.last_name} added successfully`;
         } catch (error) {
             throw error;
@@ -89,8 +110,10 @@ class EmployeeDatabase extends Database {
     }
 
     async updateEmployeeRole(employee) {
+        const query = 'UPDATE employee SET role_id=? WHERE id=?';
+        const params = [employee.role_id, employee.employee_id];
         try {
-            const results = await this.db.promise().query('UPDATE employee SET role_id=? WHERE id=?', [employee.role_id, employee.employee_id]);
+            const results = await this.executeQuery(query, params);
             return results;
         } catch (error) {
             throw error;
