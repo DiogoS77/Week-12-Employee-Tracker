daef37de9e9cde04507a29a4a5fb79e31b64af3c
DiogoS77 <diogo.messi.18@hotmail.com>
2023-06-30 01:29:20 +0100

    completed seeds.sql by adding info to employees

--- db/seeds.sql
+++ db/seeds.sql
@@ -19,3 +19,17 @@ INSERT INTO roles (id, title, salary, department_id) VALUES
   (10, 'Technical Support Specialist', 55000, 4),
   (11, 'Financial Controller', 120000, 5),
   (12, 'Legal Assistant', 60000, 6);
+
+INSERT INTO employees (id, first_name, last_name, role_id, manager_id) VALUES
+(1, 'Lionel', 'Messi', 1, NULL),
+(2, 'Cristiano', 'Ronaldo', 2, 1),
+(3, 'Neymar', NULL, 3, 1),
+(4, 'Mohamed', 'Salah', 4, 1),
+(5, 'Kylian', 'Mbappe', 5, 1),
+(6, 'Luka', 'Modric', 6, 1),
+(7, 'Robert', 'Lewandowski', 7, NULL),
+(8, 'Sergio', 'Ramos', 8, 7),
+(9, 'Kevin', 'De Bruyne', 9, 7),
+(10, 'Harry', 'Kane', 10, 7),
+(11, 'Virgil', 'van Dijk', 11, 7),
+(12, 'Manuel', 'Neuer', 12, 7);
