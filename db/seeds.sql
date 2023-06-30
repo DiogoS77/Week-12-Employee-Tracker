ee940e926aaa75b54acecfd2ee5face198c5d7c5
DiogoS77 <diogo.messi.18@hotmail.com>
2023-06-30 01:21:21 +0100

    added different title/salary to roles in seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,21 @@
-INSERT INTO departments (id, name)
-VALUES
+INSERT INTO departments (id, name) VALUES
   (1, 'Sales'),
   (2, 'Engineering'),
   (3, 'Marketing'),
   (4, 'Customer Service'),
   (5, 'Finance'),
   (6, 'Legal');
+
+INSERT INTO roles (id, title, salary, department_id) VALUES
+  (1, 'Sales Lead', 100000, 1),
+  (2, 'Software Engineer', 90000, 2),
+  (3, 'Marketing Specialist', 80000, 3),
+  (4, 'Customer Service Representative', 50000, 4),
+  (5, 'Financial Analyst', 95000, 5),
+  (6, 'Legal Counsel', 110000, 6),
+  (7, 'Account Executive', 80000, 1),
+  (8, 'Software Developer', 85000, 2),
+  (9, 'Digital Marketing Manager', 90000, 3),
+  (10, 'Technical Support Specialist', 55000, 4),
+  (11, 'Financial Controller', 120000, 5),
+  (12, 'Legal Assistant', 60000, 6);
